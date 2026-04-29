609d7ebe056521147b8b4f65603d763262749bcd
Maradana hemalatha <hemamaradana5@gmail.com>
2026-04-29 10:27:14 +0530

    debug jwt secret

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
